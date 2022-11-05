40fe662fff338425c5d4fc3f2556a09b08e03257
RahulSureshPatil <115691358+RahulSureshPatil@users.noreply.github.com>
2022-11-05 22:36:57 +0530

    Call Back Hell Task
    R0

---
+++ script.js
@@ -0,0 +1,50 @@
+let n=10
+setTimeout(()=>{
+shownumber(n);
+n--;
+setTimeout(()=>{
+shownumber(n);
+n--;
+setTimeout(()=>{
+    shownumber(n);
+    n--;
+setTimeout(()=>{
+    shownumber(n);
+    n--;
+setTimeout(()=>{
+    shownumber(n);
+    n--;
+setTimeout(()=>{
+    shownumber(n);
+    n--;
+setTimeout(()=>{
+    shownumber(n);
+    n--;
+setTimeout(()=>{
+    shownumber(n);
+    n--;
+setTimeout(()=>{
+    shownumber(n);
+    n--;
+setTimeout(()=>{
+    shownumber(n);
+    n--;
+setTimeout(()=>{
+    console.log=document.getElementById("demo").innerHTML = '"Happy 75th Independence Day"'
+},1000)    
+},1000)
+},1000)    
+},1000)    
+},1000)    
+},1000)    
+},1000)    
+},1000)    
+},1000)
+},1000)
+},1000)
+
+function shownumber(number){
+    document.getElementById("demo").innerHTML = number
+}
+
+
